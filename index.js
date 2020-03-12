e3c5ac04b7e84bc6b609e0245ec07769f9a34d94
Luis PM <luisp237@gmail.com>
2020-03-12 19:12:47 -0400

    initialize server.js and index.js inside user

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* Go to https://www.youtube.com/watch?v=dQw4w9WgXcQ and follow the instructions and code! */
+
+
+const server = require('./server.js');
+
+const PORT = 5010;
+
+server.listen(PORT, () => {
+    console.log(`\n ***The server is running at http://localhost:${PORT} \n`);
+})
